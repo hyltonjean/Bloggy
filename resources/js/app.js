ed5b3e5137882ce811229415dc968c962cea6ed3
hyltonjean <hyltonjean@gmail.com>
2019-09-21 15:52:05 +0200

    Configured js and css using npm and setup database

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-require('./bootstrap');
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require("./bootstrap");
+
+window.Vue = require("vue");
+import Buefy from "buefy";
+
+Vue.use(Buefy);
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
